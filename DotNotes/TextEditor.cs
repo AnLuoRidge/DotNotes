626e4ad5dcf37cc572172b25d0dbfa72415cc2b4
Steven Ding <anluoridge@gmail.com>
2018-10-05 21:36:03 +1000

    writing open/close file func

--- DotNotes/TextEditor.cs
+++ DotNotes/TextEditor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -33,6 +34,59 @@ namespace DotNotes
             this.Hide();
 
             lf.Show();
+        }
+
+        private void open()
+        {
+            try 
+            {
+                string line;
+//Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("/Users/anluoridge/Library/Mobile Documents/com~apple~CloudDocs/UTS/dotnet/Assignment2/login.txt");
+
+//Read the first line of text
+                line = sr.ReadLine();
+
+//Continue to read until you reach end of file
+                while (line != null) 
+                {
+//write the lie to console window
+//                    Console.WriteLine(line);
+                    // jimy1,asdasd,View,Jim,MacDonald,14-09-1970
+                    var userInfo = line.Split(',');
+                    var username = userInfo[0];
+                    var pwd = userInfo[1];
+                    var type = userInfo[2];
+                    var firstName = userInfo[3];
+                    var lastName = userInfo[4];
+                    var dateStr = userInfo[5];
+
+                    UserType userType = type == "Edit" ? UserType.Edit : UserType.View;
+
+                    var user = new User(username, pwd, firstName, lastName, dateStr, userType);
+                    Console.WriteLine(user.Username+user.DateOfBirth+user.Password+user.FirstName);
+                    // type enum
+                    // date convert
+//Read the next line
+                    line = sr.ReadLine();
+                }
+
+                
+//close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+            
+            // add a line to end of file
+
         }
 
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
