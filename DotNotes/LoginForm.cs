a5efe444c3e672f80515bb1ab1f0586669f80612
Alan Ridge <anluoridge@gmail.com>
2018-10-05 22:25:41 +1000

    Add user login func

--- DotNotes/LoginForm.cs
+++ DotNotes/LoginForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +13,67 @@ namespace DotNotes
 {
     public partial class LoginForm : Form
     {
+        private List<User> _users;
         public LoginForm()
         {
             InitializeComponent();
             this.CenterToScreen();
+            _users = LoadUsers();
+        }
+
+        static List<User> LoadUsers()
+        {
+            var users = new List<User>();
+            try
+            {
+                string line;
+                //Pass the file path and file name to the StreamReader constructor
+                string dir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                StreamReader sr = new StreamReader(dir + @"\login.txt");
+
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the lie to console window
+                    //                    Console.WriteLine(line);
+                    // jimy1,asdasd,View,Jim,MacDonald,14-09-1970
+                    var userInfo = line.Split(',');
+                    var username = userInfo[0];
+                    var pwd = userInfo[1];
+                    var type = userInfo[2];
+                    var firstName = userInfo[3];
+                    var lastName = userInfo[4];
+                    var dateStr = userInfo[5];
+
+                    UserType userType = type == "Edit" ? UserType.Edit : UserType.View;
+
+                    var user = new User(username, pwd, firstName, lastName, dateStr, userType);
+                    users.Add(user);
+                    // type enum
+                    // date convert
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+
+                //close the file
+                sr.Close();
+                return users;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return users;
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+            // add a line to end of file
 
         }
 
@@ -23,11 +81,25 @@ namespace DotNotes
         {
             // transion to Editor
             // UserType type 
-            var te = new TextEditor(UserType.Edit);
+            var username = usernameTextBox.Text;
+            var password = passwordTextBox.Text;
 
-            Hide();
-            
-            te.Show();
+            User authorisedUser;
+            try
+            {
+                authorisedUser = _users.Single(s => s.Username == username && s.Password == password);
+
+                var te = new TextEditor(authorisedUser.Type);
+
+                Hide();
+
+                te.Show();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed. Please try again.");
+            }
+
         }
 
         private void newUserButton_Click(object sender, EventArgs e)
--- DotNotes/Program.cs
+++ DotNotes/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
 namespace DotNotes
 {
@@ -20,58 +19,5 @@ namespace DotNotes
             Application.Run(new LoginForm());
         }
 
-        static void LoadUsers()
-        {
-            try
-            {
-                string line;
-                //Pass the file path and file name to the StreamReader constructor
-                string dir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-                StreamReader sr = new StreamReader(dir + @"\login.txt");
-
-                //Read the first line of text
-                line = sr.ReadLine();
-
-                //Continue to read until you reach end of file
-                while (line != null)
-                {
-                    //write the lie to console window
-                    //                    Console.WriteLine(line);
-                    // jimy1,asdasd,View,Jim,MacDonald,14-09-1970
-                    var userInfo = line.Split(',');
-                    var username = userInfo[0];
-                    var pwd = userInfo[1];
-                    var type = userInfo[2];
-                    var firstName = userInfo[3];
-                    var lastName = userInfo[4];
-                    var dateStr = userInfo[5];
-
-                    UserType userType = type == "Edit" ? UserType.Edit : UserType.View;
-
-                    var user = new User(username, pwd, firstName, lastName, dateStr, userType);
-                    Console.WriteLine(user.Username + user.DateOfBirth + user.Password + user.FirstName);
-                    // type enum
-                    // date convert
-                    //Read the next line
-                    line = sr.ReadLine();
-                }
-
-
-                //close the file
-                sr.Close();
-                Console.ReadLine();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Executing finally block.");
-            }
-
-            // add a line to end of file
-
-        }
     }
 }
